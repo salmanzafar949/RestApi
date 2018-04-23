3ca38dc896bd73573388c01e074a61c90700d068
salman zafar <salmanzafar949@gmail.com>
2018-04-23 06:53:11 +0500

    Product Collection implemented

--- app/Http/Resources/ProductCollection.php
+++ app/Http/Resources/ProductCollection.php
@@ -14,6 +14,12 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'totalPrice' => round( 1- ($this->discount/100)*$this->price),
+            'rating' => $this->Reviews->count > 0 ? round($this->Reviews->sum()/$this->Reviews->count(),2) : 'No Reviews',
+            'href' => [
+                'link' => route('products.show',$this->id)
+            ];
     }
 }
