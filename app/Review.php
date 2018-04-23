1b0e19a620cceceab5dd81b5d305272bea3a3eb4
salman zafar <salmanzafar949@gmail.com>
2018-04-24 03:35:34 +0500

    Completed Reviews Create part

--- app/Review.php
+++ app/Review.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    protected $fillable = ['product_id','customer','comment','rating'];
 
     public function Product()
     {
