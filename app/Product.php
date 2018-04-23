4cfc0d264250adde7cf7a7fbf049bbca6edc6f43
salman zafar <salmanzafar949@gmail.com>
2018-04-23 06:09:58 +0500

    Define Relationship

--- app/Product.php
+++ app/Product.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App;
-
+use App\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     //
+
+    public function Reviews()
+    {
+        return
+            $this
+                ->hasMany(Review::class);
+    }
 }
--- app/Review.php
+++ app/Review.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App;
-
+use App\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
     //
+
+    public function Product()
+    {
+        return
+            $this
+                ->belongsTo(Product::class);
+    }
 }
