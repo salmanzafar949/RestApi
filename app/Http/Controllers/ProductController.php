957c324acb31040c11d35968e4be416462a26b7f
salman zafar <salmanzafar949@gmail.com>
2018-04-24 03:09:57 +0500

    Delete Product Completed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -92,6 +92,10 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return response([
+            'data' => 'Product Deleted'
+        ],Response::HTTP_NO_CONTENT);
     }
 }
