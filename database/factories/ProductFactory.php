20a247b165aaee674137959c75ee87512b95db3a
salman zafar <salmanzafar949@gmail.com>
2018-04-23 06:03:18 +0500

    Created factories for seeding the db

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
-        //
+
+        'name' => $faker->word,
+        'description' => $faker->paragraph,
+        'price' => $faker->numberBetween(10,1000),
+        'stock' => $faker->numberBetween(1,100),
+        'discount' => $faker->numberBetween(10,600)
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Product;
 use Faker\Generator as Faker;
 
 $factory->define(App\Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => function(){
+        return Product::all()->random();
+        },
+        'customer' => $faker->name,
+        'comment' => $faker->paragraph,
+        'rating' => $faker->numberBetween(0,5)
     ];
 });
